8301c6bf6f587ebdcd50d55fe433721adbf06b19
Sayan Das <poke19962008@gmail.com>
2015-07-04 12:52:34 +0530

    Initial Commit

---
+++ iCamel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iCamel
+//
+//  Created by Sayan Das on 04/07/15.
+//  Copyright © 2015 Sayan Das. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
